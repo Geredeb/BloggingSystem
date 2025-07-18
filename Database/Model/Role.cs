1b3c5bb5dc43bd02d91139e06dbab0e4da9b4deb
Tamim Khan <104095793+Geredeb@users.noreply.github.com>
2025-07-18 16:13:42 +0600

    Add files via upload

--- Database/Model/Role.cs
+++ Database/Model/Role.cs
@@ -7,6 +7,6 @@ namespace Database.Model
         [Key]
         public int RoleId { get; set; }
         public string Name { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive {  get; set; }
     }
 }
